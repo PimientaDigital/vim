cefd06ac047df69a2d402a2e9249441fa04f6b28
Luis Mayta <slovacus@gmail.com>
2015-01-23 11:35:02 -0500

    fixed service_name

--- manifests/init.pp
+++ manifests/init.pp
@@ -8,18 +8,11 @@
 #   Explanation of what this parameter affects and what it defaults to.
 #
 class vim (
-  $package_name = $::vim::params::package_name,
-  $service_name = $::vim::params::service_name,
+  $package_name = $::vim::params::package_name
 ) inherits ::module::params {
 
   # validate parameters here
 
   class { '::vim::install': } ->
-  Class['::module']
-}
-class vim {
-    package { 'vim':
-        ensure => installed,
-        require => Exec["manager update"],
-    }
+    Class['::module']
 }
